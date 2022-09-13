2f64e0fb462deb38e320da980241998aa8fb3837
juanib12 <bjuan.ignacio11@gmail.com>
2022-09-13 17:14:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Countdown from "./Countdown.js";
+import logo from "./img/argentina.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const currentDate = new Date();
+    const year =
+      currentDate.getMonth() === 11 && currentDate.getDate() > 23
+        ? currentDate.getFullYear() + 1
+        : currentDate.getFullYear();
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Cuenta Regresiva Mundial 2022</h2>
+        </div>
+        {/* <h3 className="title">
+          Lo que falta para que la SCALONETA detone colas europeas!:
+        </h3> */}
+
+        <Countdown date={`${year}-11-20T13:00:00`} />
+      </div>
+    );
+  }
 }
 
 export default App;
